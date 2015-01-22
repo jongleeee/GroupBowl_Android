abc0e1fbbcd6a3efb9e558d6dbc6153ce969fc2b
jongleee <lee.jongyun13@gmail.com>
2015-01-22 15:43:27 -0800

    loaded groupList

--- app/src/main/java/com/heapstack/groupbowl/SelectGroupActivity.java
+++ app/src/main/java/com/heapstack/groupbowl/SelectGroupActivity.java
@@ -1,33 +1,83 @@
 package com.heapstack.groupbowl;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ProgressBar;
 
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 import java.lang.reflect.Array;
+import java.util.List;
+import java.util.Objects;
 
 
-public class SelectGroupActivity extends Activity {
+public class SelectGroupActivity extends ListActivity {
 
     public static Array userGroup;
+    private ProgressBar spinner;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        spinner = (ProgressBar) findViewById(R.id.progressBarMember);
+        spinner.setVisibility(View.GONE);
         setContentView(R.layout.activity_select_group);
     }
+
+
 
     @Override
     public void onResume() {
         super.onResume();
 
-        userGroup = CurrentMember.getUserGroup();
 
-        String[] groupList
+        String userEmail = CurrentMember.getUserEmail();
+
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+        query.whereEqualTo("username", userEmail);
+        spinner.setVisibility(View.VISIBLE);
+        query.getFirstInBackground(new GetCallback<ParseUser>() {
+            @Override
+            public void done(ParseUser parseUser, ParseException e) {
+
+                spinner.setVisibility(View.GONE);
+
+                if (e == null) {
+                    userGroup = (Array) parseUser.get("groups");
+
+                    String[] userGroups = new String[Array.getLength(userGroup)];
+
+                    for (int i = 0; i < Array.getLength(userGroup); i++) {
+
+                        userGroups[i] = (String) Array.get(userGroup, i);
+                    }
+
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(SelectGroupActivity.this,
+                            android.R.layout.simple_list_item_1, userGroups);
+                    setListAdapter(adapter);
+
+
+
+                } else {
+
+                }
+            }
+
+        });
+
+
+
+
 
     }
 
