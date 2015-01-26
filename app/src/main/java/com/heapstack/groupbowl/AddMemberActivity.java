bf58813c90d6208b8132de1ddbfaedc601a975d4
jongleee <lee.jongyun13@gmail.com>
2015-01-26 02:00:40 -0800

    added Member Adding feature

--- app/src/main/java/com/heapstack/groupbowl/AddMemberActivity.java
+++ app/src/main/java/com/heapstack/groupbowl/AddMemberActivity.java
@@ -1,17 +1,186 @@
 package com.heapstack.groupbowl;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
 import com.heapstack.groupbowl.R;
+import com.parse.FunctionCallback;
+import com.parse.GetCallback;
+import com.parse.ParseCloud;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class AddMemberActivity extends Activity {
+
+    protected EditText mMemberEmail;
+    protected Button mMemberSubmit;
+    protected String currentMember;
+    protected String memberEmail;
+    protected ArrayList<String> groupList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_member);
+
+        mMemberEmail = (EditText)findViewById(R.id.memberEmailField);
+        mMemberSubmit = (Button)findViewById(R.id.addMemberButton);
+
+        mMemberSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                memberEmail = mMemberEmail.getText().toString().trim();
+
+                if (memberEmail.isEmpty()) {
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(AddMemberActivity.this);
+                    builder.setMessage("Please fill out all the information.")
+                            .setTitle("Oops!")
+                            .setPositiveButton(android.R.string.ok, null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                } else {
+
+                    // check if there is a user
+                    ParseQuery<ParseUser> userQuery = ParseUser.getQuery();
+                    userQuery.whereEqualTo("email", memberEmail);
+                    userQuery.getFirstInBackground(new GetCallback<ParseUser>() {
+                        @Override
+                        public void done(final ParseUser user, ParseException e) {
+                            if (user != null) {
+
+                                // check if the user already added
+                                currentMember = CurrentGroup.getCurrentGroupName() + ParseConstants.MEMBER;
+
+                                groupList = (ArrayList) user.get("groups");
+                                if (groupList == null) {
+                                    groupList = new ArrayList<String>();
+                                    groupList.add(CurrentGroup.getCurrentGroupName());
+                                } else {
+                                    groupList.add(CurrentGroup.getCurrentGroupName());
+                                }
+
+                                ParseQuery<ParseObject> objectQuery = ParseQuery.getQuery(currentMember);
+                                objectQuery.whereEqualTo("email", memberEmail);
+                                objectQuery.getFirstInBackground(new GetCallback<ParseObject>() {
+                                    @Override
+                                    public void done(final ParseObject parseObject, ParseException e) {
+                                        if (e == null) {
+
+                                            AlertDialog.Builder builder = new AlertDialog.Builder(AddMemberActivity.this);
+                                            builder.setMessage("Already added")
+                                                    .setTitle("Oops!")
+                                                    .setPositiveButton(android.R.string.ok, null);
+                                            AlertDialog dialog = builder.create();
+                                            dialog.show();
+
+                                        } else {
+
+                                            // Change user groupList
+                                            HashMap<String, Object> params = new HashMap<String, Object>();
+                                            params.put("userID", memberEmail);
+                                            params.put("groupList", groupList);
+
+                                            ParseCloud.callFunctionInBackground("updateMember", params, new FunctionCallback<Object>() {
+                                                @Override
+                                                public void done(Object o, ParseException e) {
+                                                    if (e == null) {
+
+                                                        // add User to the group
+                                                        ParseObject groupMember = new ParseObject(currentMember);
+
+                                                        groupMember.put("name", user.get("name"));
+                                                        groupMember.put("email", user.get("email"));
+                                                        groupMember.put("phone", user.get("phone"));
+                                                        groupMember.put("title", "Member");
+
+                                                        groupMember.saveInBackground(new SaveCallback() {
+                                                            @Override
+                                                            public void done(ParseException e) {
+                                                                if (e == null) {
+
+                                                                    AlertDialog.Builder builder = new AlertDialog.Builder(AddMemberActivity.this);
+                                                                    builder.setMessage("Member has been added!")
+                                                                            .setTitle("Succeed!")
+                                                                            .setPositiveButton(android.R.string.ok, null);
+                                                                    AlertDialog dialog = builder.create();
+                                                                    dialog.show();
+
+                                                                    Intent intent = new Intent(AddMemberActivity.this, MainActivity.class);
+                                                                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                                                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                                                    startActivity(intent);
+
+
+
+                                                                } else {
+
+                                                                    AlertDialog.Builder builder = new AlertDialog.Builder(AddMemberActivity.this);
+                                                                    builder.setMessage("Error. Contact the administrator.")
+                                                                            .setTitle("Oops!")
+                                                                            .setPositiveButton(android.R.string.ok, null);
+                                                                    AlertDialog dialog = builder.create();
+                                                                    dialog.show();
+
+                                                                }
+                                                            }
+                                                        });
+
+
+                                                    } else {
+
+                                                        AlertDialog.Builder builder = new AlertDialog.Builder(AddMemberActivity.this);
+                                                        builder.setMessage("Error. Please try again.")
+                                                                .setTitle("Oops!")
+                                                                .setPositiveButton(android.R.string.ok, null);
+                                                        AlertDialog dialog = builder.create();
+                                                        dialog.show();
+
+                                                    }
+                                                }
+                                            });
+
+
+                                        }
+                                    }
+                                });
+
+
+                            } else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(AddMemberActivity.this);
+                                builder.setMessage("Not Found")
+                                        .setTitle("Oops!")
+                                        .setPositiveButton(android.R.string.ok, null);
+                                AlertDialog dialog = builder.create();
+                                dialog.show();
+                            }
+                        }
+                    });
+
+
+
+                }
+
+            }
+        });
+
     }
 
 
--- app/src/main/java/com/heapstack/groupbowl/CreateEventPaymentActivity.java
+++ app/src/main/java/com/heapstack/groupbowl/CreateEventPaymentActivity.java
@@ -1,17 +1,112 @@
 package com.heapstack.groupbowl;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.SaveCallback;
+
+import java.util.Date;
 
 
 public class CreateEventPaymentActivity extends Activity {
+
+    protected EditText mEventTitle;
+    protected EditText mEventVenmoEmail;
+    protected EditText mEventContext;
+    protected EditText mEventFee;
+    protected DatePicker mEventDate;
+    protected TimePicker mEventTime;
+    protected Button mEventSubmit;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event_payment);
+
+
+        mEventTitle = (EditText)findViewById(R.id.eventTitleField);
+        mEventVenmoEmail = (EditText)findViewById(R.id.eventVenmoEmailField);
+        mEventContext = (EditText)findViewById(R.id.eventContextField);
+        mEventSubmit = (Button)findViewById(R.id.eventPaymentSubmit);
+        mEventFee = (EditText)findViewById(R.id.eventFeeField);
+        mEventDate = (DatePicker)findViewById(R.id.eventDate);
+        mEventTime = (TimePicker)findViewById(R.id.eventTime);
+
+
+        mEventSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String title = mEventTitle.getText().toString().trim();
+                String venmoEmail = mEventVenmoEmail.getText().toString().trim();
+                String context = mEventContext.getText().toString().trim();
+                String fee = mEventFee.getText().toString().trim();
+
+
+                if (title.isEmpty() || venmoEmail.isEmpty() || context.isEmpty() || fee.isEmpty()) {
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(CreateEventPaymentActivity.this);
+                    builder.setMessage("Please fill out all the information.")
+                            .setTitle("Oops!")
+                            .setPositiveButton(android.R.string.ok, null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+
+                } else {
+                    String currentEvent = CurrentGroup.getCurrentGroupName() + ParseConstants.EVENT;
+                    ParseObject event = new ParseObject(currentEvent);
+
+                    Date eventDate = new Date(mEventDate.getYear(), mEventDate.getMonth(), mEventDate.getDayOfMonth(), mEventTime.getCurrentHour(), mEventTime.getCurrentMinute());
+
+                    event.put("title", title);
+                    event.put("venmoId", venmoEmail);
+                    event.put("payment", "YES");
+                    event.put("fee", Integer.parseInt(fee));
+                    event.put("contents", context);
+                    event.put("date", eventDate);
+
+
+                    event.saveInBackground(new SaveCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if (e == null) {
+
+                                Intent intent = new Intent(CreateEventPaymentActivity.this, MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(intent);
+
+                            } else {
+
+                                AlertDialog.Builder builder = new AlertDialog.Builder(CreateEventPaymentActivity.this);
+                                builder.setMessage("Error in saving.")
+                                        .setTitle("Oops!")
+                                        .setPositiveButton(android.R.string.ok, null);
+                                AlertDialog dialog = builder.create();
+                                dialog.show();
+
+                            }
+                        }
+                    });
+
+                }
+
+            }
+        });
+
+
     }
 
 
